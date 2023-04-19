a83946b2b27378124e8f9e436123216a2bb4ab29
Ludwig Sickert <ludwigsickert@gmail.com>
2023-04-19 21:31:48 +0200

    updated main script

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """main module"""
-from tatoeba import preprocess
+import os
+from tatoeba import preprocess, DATA_FOLDER
 from rude_nmt import label_german, label_korean
 
 TATOEBA_TAR = "https://object.pouta.csc.fi/Tatoeba-Challenge-v2021-08-07/deu-kor.tar"
@@ -10,14 +11,14 @@ if __name__ == "__main__":
 
     subtitle_data = preprocess.get_subtitle_dataset()
 
-    print(subtitle_data)
+    subtitle_data = subtitle_data.map(label_german.get_pos_tags, load_from_cache_file=False, batched=True)
+    subtitle_data = subtitle_data.map(label_korean.get_pos_tags, load_from_cache_file=False, batched=True)
+    subtitle_data = subtitle_data.map(label_german.annotate_formality, load_from_cache_file=False, num_proc=os.cpu_count())
+    subtitle_data = subtitle_data.map(label_korean.annotate_formality, load_from_cache_file=False, num_proc=os.cpu_count())
 
-    subtitle_data = subtitle_data.map(label_german.annotate_formality, load_from_cache_file=False)
-    subtitle_data = subtitle_data.map(label_korean.annotate_formality, load_from_cache_file=False)
+    subtitle_data.to_csv("./data/subtitle_data.csv", num_proc=os.cpu_count())
 
-    subtitle_data.to_csv("./data/subtitle_data.csv", num_proc=8)
+    subtitle_folder = DATA_FOLDER / "subtitles_labelled"
 
-    dat = subtitle_data.to_pandas()
+    subtitle_data.save_to_disk(subtitle_folder)
 
-    print(dat["de_formality"].value_counts())
-    print(dat["ko_formality"].value_counts())
