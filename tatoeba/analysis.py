75158c2ad5b8eaca0127835d185746c9e0fb6330
Ludwig Sickert <ludwigsickert@gmail.com>
2023-04-27 23:05:12 +0200

    moved plotting function

--- tatoeba/analysis.py
+++ tatoeba/analysis.py
@@ -14,17 +14,3 @@ def get_one_word_sentences(
         dataset = preprocess.get_dataset()[split]
 
     return dataset.filter(lambda ex: not re.search(".+\s.+", ex[lang]), num_proc=8)
-
-def get_formality_plot(ds: Dataset, form_col: str) -> None:
-    """plot the distribution of formality labels in the dataset"""
-    df = ds.to_pandas()
-    df[form_col].astype('category')
-    rows = len(df.index)
-    ax = df[form_col].value_counts().plot(kind='bar')
-    ax.set_ylabel("Number of Sentences")
-    for p in ax.patches:
-        b = p.get_bbox()
-        ax.annotate(str(round(p.get_height()/rows * 100,2)), ((b.x0 + b.x1)/2 - 0.16, b.y1 + 8000))
-
-    fig = ax.get_figure()
-    fig.savefig(f"{form_col}.png", bbox_inches="tight")
